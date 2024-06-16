689e6cb36b21c9ccb4bf158b3fc5675f4d395f7a
Shrinivassan <117668961+Shrinivassan@users.noreply.github.com>
2024-06-16 20:21:18 +0530

    Update hms.sql

--- hms.sql
+++ hms.sql
@@ -39,11 +39,11 @@ CREATE TABLE `doctors` (
 --
 
 INSERT INTO `doctors` (`did`, `email`, `doctorname`, `dept`) VALUES
-(1, 'anees@gmail.com', 'anees', 'Cardiologists'),
-(2, 'amrutha@gmail.com', 'amrutha bhatta', 'Dermatologists'),
-(3, 'aadithyaa@gmail.com', 'aadithyaa', 'Anesthesiologists'),
-(4, 'anees@gmail', 'anees', 'Endocrinologists'),
-(5, 'aneeqah@gmail.com', 'aneekha', 'corona');
+(1, 'shrinivassan7@gmail.com', 'shrini', 'Cardiologists'),
+(2, 'shrinivassannd.22csd@kongu.edu', 'shriniv', 'Dermatologists'),
+(3, '11148.shrinivassan@gmail.com', 'shriniv', 'Anesthesiologists'),
+(4, 'shobankumarkc.22csd@kongu.edu', 'shoban', 'Endocrinologists'),
+(5, 'vishwavishwa1304@gmail.com', 'vishwa', 'corona');
 
 -- --------------------------------------------------------
 
@@ -69,16 +69,15 @@ CREATE TABLE `patients` (
 --
 
 INSERT INTO `patients` (`pid`, `email`, `name`, `gender`, `slot`, `disease`, `time`, `date`, `dept`, `number`) VALUES
-(2, 'anees1@gmail.com', 'anees1 rehman khan', 'Male1', 'evening1', 'cold1', '21:20:00', '2020-02-02', 'ortho11predict', '9874561110'),
-(5, 'patient@gmail.com', 'patien', 'Male', 'morning', 'fevr', '18:06:00', '2020-11-18', 'Cardiologists', '9874563210'),
-(7, 'patient@gmail.com', 'anees', 'Male', 'evening', 'cold', '22:18:00', '2020-11-05', 'Dermatologists', '9874563210'),
-(8, 'patient@gmail.com', 'anees', 'Male', 'evening', 'cold', '22:18:00', '2020-11-05', 'Dermatologists', '9874563210'),
-(9, 'aneesurrehman423@gmail.com', 'anees', 'Male', 'morning', 'cold', '17:27:00', '2020-11-26', 'Anesthesiologists', '9874563210'),
-(10, 'anees@gmail.com', 'anees', 'Male', 'evening', 'fever', '16:25:00', '2020-12-09', 'Cardiologists', '9874589654'),
-(15, 'khushi@gmail.com', 'khushi', 'Female', 'morning', 'corona', '20:42:00', '2021-01-23', 'Anesthesiologists', '9874563210'),
-(16, 'khushi@gmail.com', 'khushi', 'Female', 'evening', 'fever', '15:46:00', '2021-01-31', 'Endocrinologists', '9874587496'),
-(17, 'aneeqah@gmail.com', 'aneeqah', 'Female', 'evening', 'fever', '15:48:00', '2021-01-23', 'Endocrinologists', '9874563210');
-
+(2, 'shrinivassan@gmail.com', 'Shrinivassan', 'Male', 'evening', 'cold', '21:20:00', '2020-02-02', 'Orthopedics', '9874561110'),
+(5, 'patient2@gmail.com', 'Vishwa', 'Male', 'morning', 'fever', '18:06:00', '2020-11-18', 'Cardiologists', '9874563210'),
+(7, 'patient3@gmail.com', 'Shoban', 'Male', 'evening', 'cold', '22:18:00', '2020-11-05', 'Dermatologists', '9874563210'),
+(8, 'patient4@gmail.com', 'Shrinivassan', 'Male', 'evening', 'cold', '22:18:00', '2020-11-05', 'Dermatologists', '9874563210'),
+(9, 'shrinivassan423@gmail.com', 'Shrinivassan', 'Male', 'morning', 'cold', '17:27:00', '2020-11-26', 'Anesthesiologists', '9874563210'),
+(10, 'vishwa@gmail.com', 'Vishwa', 'Male', 'evening', 'fever', '16:25:00', '2020-12-09', 'Cardiologists', '9874589654'),
+(15, 'khushi@gmail.com', 'Khushi', 'Female', 'morning', 'corona', '20:42:00', '2021-01-23', 'Anesthesiologists', '9874563210'),
+(16, 'khushi2@gmail.com', 'Khushi', 'Female', 'evening', 'fever', '15:46:00', '2021-01-31', 'Endocrinologists', '9874587496'),
+(17, 'aneeqah@gmail.com', 'Aneeqah', 'Female', 'evening', 'fever', '15:48:00', '2021-01-23', 'Endocrinologists', '9874563210');
 --
 -- Triggers `patients`
 --
@@ -112,7 +111,7 @@ CREATE TABLE `test` (
 --
 
 INSERT INTO `test` (`id`, `name`, `email`) VALUES
-(1, 'ANEES', 'ARK@GMAIL.COM'),
+(1, 'shrini', 'shrinivassan7@GMAIL.COM'),
 (2, 'test', 'test@gmail.com');
 
 -- --------------------------------------------------------
@@ -135,25 +134,25 @@ CREATE TABLE `trigr` (
 --
 
 INSERT INTO `trigr` (`tid`, `pid`, `email`, `name`, `action`, `timestamp`) VALUES
-(1, 12, 'anees@gmail.com', 'ANEES', 'PATIENT INSERTED', '2020-12-02 16:35:10'),
-(2, 11, 'anees@gmail.com', 'anees', 'PATIENT INSERTED', '2020-12-02 16:37:34'),
-(3, 10, 'anees@gmail.com', 'anees', 'PATIENT UPDATED', '2020-12-02 16:38:27'),
-(4, 11, 'anees@gmail.com', 'anees', 'PATIENT UPDATED', '2020-12-02 16:38:33'),
-(5, 12, 'anees@gmail.com', 'ANEES', 'Patient Deleted', '2020-12-02 16:40:40'),
-(6, 11, 'anees@gmail.com', 'anees', 'PATIENT DELETED', '2020-12-02 16:41:10'),
-(7, 13, 'testing@gmail.com', 'testing', 'PATIENT INSERTED', '2020-12-02 16:50:21'),
-(8, 13, 'testing@gmail.com', 'testing', 'PATIENT UPDATED', '2020-12-02 16:50:32'),
-(9, 13, 'testing@gmail.com', 'testing', 'PATIENT DELETED', '2020-12-02 16:50:57'),
-(10, 14, 'aneeqah@gmail.com', 'aneeqah', 'PATIENT INSERTED', '2021-01-22 15:18:09'),
-(11, 14, 'aneeqah@gmail.com', 'aneeqah', 'PATIENT UPDATED', '2021-01-22 15:18:29'),
-(12, 14, 'aneeqah@gmail.com', 'aneeqah', 'PATIENT DELETED', '2021-01-22 15:41:48'),
-(13, 15, 'khushi@gmail.com', 'khushi', 'PATIENT INSERTED', '2021-01-22 15:43:02'),
-(14, 15, 'khushi@gmail.com', 'khushi', 'PATIENT UPDATED', '2021-01-22 15:43:11'),
-(15, 16, 'khushi@gmail.com', 'khushi', 'PATIENT INSERTED', '2021-01-22 15:43:37'),
-(16, 16, 'khushi@gmail.com', 'khushi', 'PATIENT UPDATED', '2021-01-22 15:43:49'),
-(17, 17, 'aneeqah@gmail.com', 'aneeqah', 'PATIENT INSERTED', '2021-01-22 15:44:41'),
-(18, 17, 'aneeqah@gmail.com', 'aneeqah', 'PATIENT UPDATED', '2021-01-22 15:44:52'),
-(19, 17, 'aneeqah@gmail.com', 'aneeqah', 'PATIENT UPDATED', '2021-01-22 15:44:59');
+(1, 12, 'shrinivassan@gmail.com', 'Shrinivassan', 'PATIENT INSERTED', '2020-12-02 16:35:10'),
+(2, 11, 'shrinivassan@gmail.com', 'Shrinivassan', 'PATIENT INSERTED', '2020-12-02 16:37:34'),
+(3, 10, 'shrinivassan@gmail.com', 'Shrinivassan', 'PATIENT UPDATED', '2020-12-02 16:38:27'),
+(4, 11, 'shrinivassan@gmail.com', 'Shrinivassan', 'PATIENT UPDATED', '2020-12-02 16:38:33'),
+(5, 12, 'shrinivassan@gmail.com', 'Shrinivassan', 'PATIENT DELETED', '2020-12-02 16:40:40'),
+(6, 11, 'shrinivassan@gmail.com', 'Shrinivassan', 'PATIENT DELETED', '2020-12-02 16:41:10'),
+(7, 13, 'vishwa@gmail.com', 'Vishwa', 'PATIENT INSERTED', '2020-12-02 16:50:21'),
+(8, 13, 'vishwa@gmail.com', 'Vishwa', 'PATIENT UPDATED', '2020-12-02 16:50:32'),
+(9, 13, 'vishwa@gmail.com', 'Vishwa', 'PATIENT DELETED', '2020-12-02 16:50:57'),
+(10, 14, 'aneeqah@gmail.com', 'Aneeqah', 'PATIENT INSERTED', '2021-01-22 15:18:09'),
+(11, 14, 'aneeqah@gmail.com', 'Aneeqah', 'PATIENT UPDATED', '2021-01-22 15:18:29'),
+(12, 14, 'aneeqah@gmail.com', 'Aneeqah', 'PATIENT DELETED', '2021-01-22 15:41:48'),
+(13, 15, 'khushi@gmail.com', 'Khushi', 'PATIENT INSERTED', '2021-01-22 15:43:02'),
+(14, 15, 'khushi@gmail.com', 'Khushi', 'PATIENT UPDATED', '2021-01-22 15:43:11'),
+(15, 16, 'khushi2@gmail.com', 'Khushi', 'PATIENT INSERTED', '2021-01-22 15:43:37'),
+(16, 16, 'khushi2@gmail.com', 'Khushi', 'PATIENT UPDATED', '2021-01-22 15:43:49'),
+(17, 17, 'aneeqah@gmail.com', 'Aneeqah', 'PATIENT INSERTED', '2021-01-22 15:44:41'),
+(18, 17, 'aneeqah@gmail.com', 'Aneeqah', 'PATIENT UPDATED', '2021-01-22 15:44:52'),
+(19, 17, 'aneeqah@gmail.com', 'Aneeqah', 'PATIENT UPDATED', '2021-01-22 15:44:59');
 
 -- --------------------------------------------------------
 
@@ -174,7 +173,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `usertype`, `email`, `password`) VALUES
-(13, 'anees', 'Doctor', 'anees@gmail.com', 'pbkdf2:sha256:150000$xAKZCiJG$4c7a7e704708f86659d730565ff92e8327b01be5c49a6b1ef8afdf1c531fa5c3'),
+(13, 'shrinivassan', 'Doctor', 'shrinivassan@gmail.com', 'pbkdf2:sha256:150000$xAKZCiJG$4c7a7e704708f86659d730565ff92e8327b01be5c49a6b1ef8afdf1c531fa5c3'),
 (14, 'aneeqah', 'Patient', 'aneeqah@gmail.com', 'pbkdf2:sha256:150000$Yf51ilDC$028cff81a536ed9d477f9e45efcd9e53a9717d0ab5171d75334c397716d581b8'),
 (15, 'khushi', 'Patient', 'khushi@gmail.com', 'pbkdf2:sha256:150000$BeSHeRKV$a8b27379ce9b2499d4caef21d9d387260b3e4ba9f7311168b6e180a00db91f22');
 
